d90415bc28ea2d7881fa77925b94ceaf57dc139a
emmy-bradfield <emily-bradfield@outlook.com>
2022-08-15 16:54:38 +0100

    Frontend expanded to include view(id)

--- Frontend_workdir/scripts/main.js
+++ Frontend_workdir/scripts/main.js
@@ -11,6 +11,9 @@ let updateName = document.querySelector("#updateName");
 let updateEmail = document.querySelector("#updateEmail");
 let updateBtn = document.querySelector("#updateBtn");
 
+let viewID = document.querySelector("#viewID");
+let viewBtn = document.querySelector("#viewBtn");
+
 let deleteID = document.querySelector("#deleteID");
 let deleteBtn = document.querySelector("#deleteBtn");
 
@@ -54,6 +57,40 @@ let viewAll = () => {
         }).catch(err => { console.log(err); });
 }
 
+let viewer = () => {
+    let id = viewID.value;
+    axios.get(`http://localhost:8080/view?id=${id}`)
+    .then(res => {
+        let result = res.data;
+        view.innerHTML="";
+        let singleView = document.createElement("ul");
+        let refresh = document.createElement("button");
+        refresh.setAttribute("class", "btn btn-light right-btn fa-solid fa-arrow-rotate-right");
+        refresh.addEventListener("click", viewAll)
+        let title = document.createElement("h6");
+        let titleTwo = document.createElement("h6");
+        let lineOne = document.createElement("li");
+        let lineTwo = document.createElement("li");
+        let lineThree = document.createElement("li");
+        let lineFour = document.createElement("li");
+        title.textContent = `Guest #${result.id}: ${result.name}`
+        titleTwo.textContent = `(${result.email})`;
+        lineOne.textContent = `Is ${result.name}'s account activated | ${result.active}` 
+        lineTwo.textContent = `Are they able to attend | ${result.attend}`
+        lineThree.textContent = `Do they need accomodation | ${result.accom}`
+        lineFour.textContent = ` Do they wish to purchase a parking permit | ${result.park}`;
+        
+        singleView.appendChild(lineOne);
+        singleView.appendChild(lineTwo);
+        singleView.appendChild(lineThree);
+        singleView.appendChild(lineFour);
+        view.appendChild(title);
+        view.appendChild(titleTwo);
+        view.appendChild(singleView);
+        view.appendChild(refresh);
+    })
+}
+
 let update = () => {
     let id = updateID.value;
     let guest = {
@@ -83,3 +120,4 @@ let details = () => {
 createBtn.addEventListener("click", create);
 updateBtn.addEventListener("click", update);
 deleteBtn.addEventListener("click", remove);
+viewBtn.addEventListener("click", viewer);
