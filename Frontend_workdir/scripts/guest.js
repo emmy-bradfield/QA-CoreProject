0e1761412a452846a1fc432bf3098ad4a6bda3eb
emmy-bradfield <emily-bradfield@outlook.com>
2022-08-16 15:19:05 +0100

    Frontend settup ready for javascript

--- Frontend_workdir/scripts/guest.js
+++ Frontend_workdir/scripts/guest.js
@@ -1,7 +1,9 @@
 'use strict'
 
 let detailsOut = document.querySelector("#detailsOut");
-let embedID = document.querySelector("#guestID-hidden")
+let embedID = document.querySelector("#guestID-hidden");
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 let shareDetails = () => {
     if (localStorage['details'] != null) {
@@ -11,6 +13,19 @@ let shareDetails = () => {
         detailsOut.placeholder = "When your host has added event details, they will appear here";
     }
     localStorage.removeItem['details'];
+}
+
+let importDetails = () => {
+    let id = embedID.value;
+    axios.get(`http://localhost:8080/view?id=${id}`)
+    .then( res => {
+        let user = res.data;
+        let name = user.name;
+        let email = user.email;
+        let attend = user.attend;
+        let stay = user.stay;
+        let park = user.park;
+    })
 }
 
 let startScripts = () => {
